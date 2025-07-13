f6e785c3260d733f77135ee4bda46c180b84b2de
Babar Mir <babar@example.com>
2025-07-13 22:58:55 +0530

    add date time

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,5 @@
 
-
+from datetime import datetime
 print("Hello, I am a chatbot,how can I help you?\n please enter 'help' if you don't know about me\n enter 'bye' to exit")
 
 
@@ -14,8 +14,14 @@ def Answer():
             print("I am fine")
         case "what is your name?":
             print("I am chatbot, you can call me babarbot")
+        case "what is time?":
+            print(datetime.now().strftime("%H:%M:%S"))
+        case "what is date?":
+            print(f"the date is {datetime.now().strftime('%d-%m-%Y')}")
+        case "what is your favorite color?":
+            print("I don't have a favorite color, I am just a chatbot.")
         case "bye":
-            print("bye")
+            print("good bye")
         case _:
             print("I don't understand")
     print("Thank you for using this chatbot")
